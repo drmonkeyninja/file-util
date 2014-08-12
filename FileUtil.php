b4c2c374322c3211d4f3b46012a8f6b5c08acdaa
Andy Carter <andy1704@gmail.com>
2014-08-12 20:35:27 +0100

    Added FileUtil::filesize() method.

--- FileUtil.php
+++ FileUtil.php
@@ -1,6 +1,31 @@
 <?php
 
 class FileUtil {
+
+/**
+ * Returns a formatted filesize in B, KB, MB or GB
+ *
+ * @param string $path path to file to return filesize of
+ * @return string
+ */
+	public function filesize($path)
+	{
+		$bytes = @sprintf('%u', filesize($path));
+
+		if ($bytes > 0)
+		{
+			$unit = intval(log($bytes, 1024));
+			$units = array('B', 'KB', 'MB', 'GB');
+			if (array_key_exists($unit, $units) === true)
+			{
+				return sprintf('%d %s', $bytes / pow(1024, $unit), $units[$unit]);
+			}
+
+		}
+
+		return $bytes;
+	}
+
 	
 /**
  * Recursively removes a directory
@@ -33,7 +58,8 @@ class FileUtil {
 	{
 		$dir = opendir($source);
 
-		if (mkdir($dest)===true) {
+		if (mkdir($dest)===true) 
+		{
 			$files = glob($source . '/*');
 			foreach ($files as $file)
 			{
